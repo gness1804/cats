956bd68de9f0151005270e32018bc73f6b9f2548
gness1804 <graham.nessler@gmail.com>
2018-06-23 18:13:33 -0500

    Break out output into own function

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -9,13 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func showOutput(age: String) {
+        ageOutputField.text = "Your cat is " + age + " in cat years."
+    }
 
     @IBAction func onClick(_ sender: Any) {
         if ageInputField.text != "" {
             let age = ageInputField.text
             let computedAge = Int(age!)! * 7
             let ageStr = String(computedAge)
-            ageOutputField.text = "Your cat is " + ageStr + " in cat years."
+            showOutput(age: ageStr)
         }
     }
     
